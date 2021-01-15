5007e7aa9e2518634a4ccca30e16c565e7c54ef1
Michael Deyev <michael.deyev@gmail.com>
2021-01-15 09:54:24 +0300

    Initial commit

---
+++ src/main/java/com/deyevma/backendForUsersStore/BackendForUsersStoreApplication.java
@@ -0,0 +1,13 @@
+package com.deyevma.backendForUsersStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForUsersStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendForUsersStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deyevma/backendForUsersStore/BackendForUsersStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deyevma.backendForUsersStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForUsersStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
