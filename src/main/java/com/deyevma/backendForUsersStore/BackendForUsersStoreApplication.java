fdf6524884a135055e637bfaff29d7ed8cdc98bb
Michael Deyev <michael.deyev@gmail.com>
2021-01-19 10:47:11 +0300

    bugfix in login functionality.

--- src/main/java/com/deyevma/backendForUsersStore/BackendForUsersStoreApplication.java
+++ src/main/java/com/deyevma/backendForUsersStore/BackendForUsersStoreApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.deyevma.backendForUsersStore.repo")
-@EntityScan("com.deyevma.backendForUsersStore.model")
+//@EnableJpaRepositories("com.deyevma.backendForUsersStore.repo")
+//@EntityScan("com.deyevma.backendForUsersStore.model")
 @SpringBootApplication(scanBasePackages = "com.deyevma.backendForUsersStore")
 public class BackendForUsersStoreApplication {
 
